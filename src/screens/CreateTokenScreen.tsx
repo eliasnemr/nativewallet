517c3873cbd9534eb499c61e589beb7767e1d55c
Elias <eliasnemr@gmail.com>
2022-01-31 18:33:54 +0200

    Preview added
    > Added a preview card

--- src/screens/CreateTokenScreen.tsx
+++ src/screens/CreateTokenScreen.tsx
@@ -3,18 +3,29 @@ import React, {FC, useState} from 'react';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
 
-import {Button, Text, TextInput} from 'react-native-paper';
-import {ImageBackground, useColorScheme, View} from 'react-native';
+import {colorPalette} from '../themes/themes';
+
+import {
+  Button,
+  Portal,
+  Avatar,
+  Text,
+  TextInput,
+  Modal,
+  Card,
+  Divider,
+  Caption,
+} from 'react-native-paper';
+import {
+  Alert,
+  ImageBackground,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {appLayout, formStyle} from '../styles';
 
-const customToken = {
-  name: {
-    name: 'CustomToken',
-    description: 'A Description',
-    icon: 'an icon',
-  },
-  amount: 'Amount to mint',
-};
+import {tokencreate} from '../api/rpc-commands';
 
 const CreateTokenSchema = Yup.object().shape({
   name: Yup.string().required('Field Required'),
@@ -27,12 +38,43 @@ const CreateTokenScreen: FC = () => {
   const scheme = useColorScheme();
   const [btnText, setBtnText] = useState('Mint');
 
+  const [visible, setVisible] = useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+
   return (
     <Formik
       initialValues={{name: '', description: '', url: '', amount: ''}}
       validationSchema={CreateTokenSchema}
       onSubmit={(values, {resetForm}) => {
-        console.log(values);
+        // console.log(values);
+        setBtnText('Minting...');
+
+        const customToken = {
+          name: {
+            name: values.name,
+            description: values.description,
+            icon: values.url,
+          },
+          amount: values.amount,
+        };
+
+        tokencreate(customToken)
+          .then((res: any) => {
+            if (res.status) {
+              Alert.alert('Minting Progress', 'Token minted successfully.');
+
+              setBtnText('Mint');
+
+              resetForm();
+            } else {
+              throw new Error(res.message);
+            }
+          })
+          .catch(err => {
+            setBtnText('Mint');
+            Alert.alert('Minting Progress', `${err}`);
+          });
       }}>
       {({handleChange, handleBlur, handleSubmit, values, errors, touched}) => (
         <ImageBackground
@@ -90,10 +132,11 @@ const CreateTokenScreen: FC = () => {
               value={values.url}
             />
             <Button
-              mode="contained"
+              color={colorPalette.colors.accent}
+              mode="outlined"
               style={[formStyle.formBtn, {marginBottom: 10}]}
               labelStyle={formStyle.formBtnLabel}
-              onPress={handleSubmit}>
+              onPress={showModal}>
               Preview
             </Button>
             <Button
@@ -105,6 +148,40 @@ const CreateTokenScreen: FC = () => {
               {btnText}
             </Button>
           </View>
+          <Portal>
+            <Modal
+              visible={visible}
+              onDismiss={hideModal}
+              contentContainerStyle={style.container}>
+              <Card style={style.card}>
+                <Card.Title title="Preview" />
+                <Card.Cover
+                  style={{margin: 50, marginBottom: 30, marginTop: 10}}
+                  source={
+                    values.url && values.url.length > 0
+                      ? {uri: values.url}
+                      : {uri: 'https://picsum.photos/700'}
+                  }
+                />
+                <Divider />
+                <Card.Title
+                  style={style.cardTitle}
+                  title={values.name}
+                  subtitle={values.description}
+                  titleNumberOfLines={1}
+                  subtitleNumberOfLines={3}
+                />
+                <Divider />
+                <View style={style.cardCaptionWrapper}>
+                  <Avatar.Image
+                    size={24}
+                    source={require('../assets/images/minimaLogoSquare.png')}
+                  />
+                  <Caption style={style.cardCaption}>Minted on Minima</Caption>
+                </View>
+              </Card>
+            </Modal>
+          </Portal>
         </ImageBackground>
       )}
     </Formik>
@@ -112,3 +189,29 @@ const CreateTokenScreen: FC = () => {
 };
 
 export default CreateTokenScreen;
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: 'transparent',
+    padding: 30,
+  },
+  card: {
+    borderRadius: 8,
+    backgroundColor: 'rgba(255, 255, 255, 1.0)',
+  },
+  cardTitle: {
+    marginBottom: 20,
+  },
+  cardCaption: {
+    fontWeight: '800',
+    marginLeft: 5,
+    fontSize: 12,
+  },
+  cardCaptionWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 5,
+    margin: 10,
+  },
+});
