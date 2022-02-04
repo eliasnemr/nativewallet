eb160b4c4bdefbfcc268ce7800b85d3ceaaa109a
Elias <eliasnemr@gmail.com>
2022-02-04 19:38:13 +0200

    Added code text for 'rpc enable:true'

--- src/components/organisms/ServiceUnavailable.tsx
+++ src/components/organisms/ServiceUnavailable.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {appLayout} from '../../styles';
+import {colorPalette} from '../../themes/themes';
 
 const ServiceUnavailable = () => {
   return (
@@ -9,8 +10,16 @@ const ServiceUnavailable = () => {
       <Text style={appLayout.na503}>503</Text>
       <Text style={appLayout.na503s}>Service unavailable</Text>
       <Text style={appLayout.na}>
-        Try checking the status of your Minima Node, enabling your rpc by typing
-        `rpc enable:true` in the apk terminal or simply refreshing this page.
+        Try checking the status of your Minima Node, enabling your rpc by typing{' '}
+        <Text
+          style={{
+            backgroundColor: colorPalette.colors.accent,
+            margin: 15,
+            color: '#fff',
+          }}>
+          `rpc enable:true`
+        </Text>{' '}
+        in the apk terminal or simply refreshing this page.
       </Text>
     </View>
   );
